33b5e8f71f4f043adaf8c8c18a5bf70e37f57d1e
Pankaj_2008 <pankajgunjal356@gmail.com>
2026-09-20 02:56:20 +0530

    Fix frontend static file serving

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve main website files
-app.use(express.static(path.join(__dirname, "../")));
-
+app.use(express.static(path.join(__dirname, "../frontend")));
 // API Routes
 app.use("/api", orderRoutes);
 app.use("/api", portfolioRoutes);
